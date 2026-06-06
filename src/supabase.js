6c4a1f89162f40d4406d4788e59d06d478361360
GLIMMERS254 <raymondpolycarp383@gmail.com>
2026-06-06 13:12:41 +0300

    Supabase chat fix

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://pzqfgrgynfkqsmqetzbf.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6cWZncmd5bmZrcXNtcWV0emJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MTU2NDAsImV4cCI6MjA5NjI5MTY0MH0.ETyf8J58WIJbRTIN6nrdye-HNdMt1ogaEnRvInLqw4g";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
